a3ed757ee49e703d1ed841b8a7b7293bcfab8cc4
ekrolo <112668557+ekrolo@users.noreply.github.com>
2023-09-07 15:44:15 +0800

    EXERCISE 1 CS4CharmOlo.java

--- src/cs4charmolo/CS4CharmOlo.java
+++ src/cs4charmolo/CS4CharmOlo.java
@@ -3,6 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package cs4charmolo;
+
+/**
+ *
+ * @author elijahkhallel
+ */
 public class CS4CharmOlo {
 
     /**
